f377442bb2b505e716e1bfef3fe8b83a55abcb5e
Gilbert <gssenyonjo@gmail.com>
2020-03-13 17:37:31 +0300

    finished generics codelab. Going on to functional manipulation and annotations

--- src/generics/Aquarium.kt
+++ src/generics/Aquarium.kt
@@ -31,9 +31,6 @@ class Aquarium<out T: WaterSupply>(val waterSupply: T) {
     }
 }
 
-fun <T: WaterSupply> isWaterClean(aquarium: Aquarium<T>) {
-    println("Cleanliness is :${aquarium.waterSupply.needsProcessing}")
-}
 
 interface Cleaner<in T: WaterSupply> {
     fun clean(waterSupply: T)
@@ -64,9 +61,48 @@ fun genExample2() {
     aquarium.addWater(cleaner)
 }
 
+fun <T: WaterSupply> isWaterClean(aquarium: Aquarium<T>) {
+    println("Needs cleaning: ${aquarium.waterSupply.needsProcessing}")
+}
+
+// extension function using reified types
+inline fun <reified T: WaterSupply> WaterSupply.isOfType() = this is T
+
+// using star projection
+inline fun <reified R: WaterSupply> Aquarium<*>.hasWaterSupplyOfType() = waterSupply is R
+
+
+fun testGenFun() {
+    val aq = Aquarium(TapWater())
+    val aq2 = Aquarium(LakeWater())
+    aq.waterSupply.addChemicalCleaners()
+    isWaterClean(aq)
+    isWaterClean(aq2)
+}
+
+fun testGenFun2() {
+    val aq = Aquarium(TapWater())
+    println(aq.hasWaterSupplyOfType<FishStoreWater>()) // returns false
+}
+
+fun testExtFun() {
+    val aq = Aquarium(LakeWater())
+    println(aq.waterSupply.isOfType<LakeWater>())
+    println(aq.waterSupply.isOfType<TapWater>())
+    println(aq.waterSupply.isOfType<WaterSupply>())
+    println(aq.waterSupply.isOfType<FishStoreWater>())
+
+    // testing hasWaterSupplyOf
+    println(aq.hasWaterSupplyOfType<LakeWater>())
+    println(aq.hasWaterSupplyOfType<TapWater>())
+}
 
 fun main() {
 //    genericsExample()
 //    inAndOutExample()
-    genExample2()
+//    genExample2()
+//    testGenFun()
+//    testGenFun2()
+    testExtFun()
 }
+
